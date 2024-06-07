6789e5bbfd8180fedeaa3c7464bd4968e6dda9a9
JayaPriyaJayaKumarSelvi <116954229+JayaPriyaJayaKumarSelvi@users.noreply.github.com>
2024-06-07 08:59:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import Home from './Home';
+import Welcome from './Welcome';
+import AdminPage from './AdminPage';
+import NavBar from './NavBar';
+import VaccinationCenterTable from './VaccinationCenterTable'; // Import VaccinationCenter component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar /> {/* Include the NavBar component */}
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/admin-login' element={<Login isAdmin={true} />} />
+        <Route path='/admin-page' element={<AdminPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/vaccination-center' element={<VaccinationCenterTable />} /> {/* Include the VaccinationCenter route */}
+      </Routes>
+    </Router>
   );
 }
 
